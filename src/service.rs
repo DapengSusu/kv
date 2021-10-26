c539e57991b957a77cafbc2ccf449953425278d0
Jumper <911079536@qq.com>
2021-10-26 16:08:33 +0800

    添加Service结构

--- src/service.rs
+++ src/service.rs
@@ -1,9 +1,119 @@
 mod command_service;
 
-use crate::{CommandResponse, Storage};
+use std::sync::Arc;
+use tracing::debug;
+use crate::{
+    CommandRequest,
+    CommandResponse,
+    KVError,
+    MemTable,
+    Storage,
+    command_request::RequestData
+};
 
 /// 对 Command 的处理的抽象
 pub trait CommandService {
     /// 处理 Command，返回 Response
     fn execute(self, store: &impl Storage) -> CommandResponse;
+}
+
+/// Service 数据结构
+pub struct Service<Store = MemTable> {
+    inner: Arc<ServiceInner<Store>>
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self { inner: Arc::clone(&self.inner) }
+    }
+}
+
+impl<Store: Storage> Service<Store> {
+    pub fn new(store: Store) -> Self {
+        Self { inner: Arc::new(ServiceInner { store }) }
+    }
+
+    pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
+        debug!("Got request: {:?}", cmd);
+        // TODO: 发送 on_received 事件
+
+        let res = dispatch(cmd, &self.inner.store);
+        debug!("Executed response: {:?}", res);
+        // TODO: 发送 on_executed 事件
+
+        res
+    }
+}
+
+// 从 Request 中得到 Response，目前处理 HSET，HGET，HGETALL
+pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
+    match cmd.request_data {
+        Some(RequestData::Hset(param)) => param.execute(store),
+        Some(RequestData::Hget(param)) => param.execute(store),
+        Some(RequestData::Hgetall(param)) => param.execute(store),
+        None => KVError::InvalidCommand("Request has no data".into()).into(),
+        _ => KVError::InternalError("Not implemented".into()).into(),
+    }
+}
+
+/// Service 内部数据结构
+pub struct ServiceInner<Store> {
+    store: Store
+}
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+    use super::*;
+    use crate::MemTable;
+
+    #[test]
+    fn service_should_work() {
+        // 我们需要一个 service 结构至少包含 storage
+        let service = Service::new(MemTable::default());
+
+        // service 可以运行在多线程环境里，它的 clone 应该是轻量级的
+        let cloned = service.clone();
+
+        // 创建一个线程，在 table t1 里写入 k1，v1
+        let handle = thread::spawn(move || {
+            let res = cloned.execute(
+                CommandRequest::new_hset("t1", "k1", "v1".into())
+            );
+
+            assert_res_ok(res, &[Value::default()], &[]);
+        });
+
+        handle.join().unwrap();
+
+        // 在当前线程读取 table t1 的 k1，应该返回 v1
+        let res = service.execute(
+            CommandRequest::new_hget("t1", "k1")
+        );
+
+        assert_res_ok(res, &["v1".into()], &[]);
+    }
+}
+
+#[cfg(test)]
+use crate::{Value, KvPair};
+
+// 测试成功返回的结果
+#[cfg(test)]
+pub fn assert_res_ok(mut res: CommandResponse, values: &[Value], pairs: &[KvPair]) {
+    res.pairs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    assert_eq!(res.status, 200);
+    assert_eq!(res.message, "");
+    assert_eq!(res.values, values);
+    assert_eq!(res.pairs, pairs);
+}
+
+// 测试失败返回的结果
+#[cfg(test)]
+pub fn assert_res_error(res: CommandResponse, code: u32, msg: &str) {
+    assert_eq!(res.status, code);
+    assert!(res.message.contains(msg));
+    assert_eq!(res.values, &[]);
+    assert_eq!(res.pairs, &[]);
 }
